27b3d8d13647dadf91d5775faa533364bcee2dae
hansh0101 <hansh0101@naver.com>
2022-07-26 22:52:16 +0900

    [FEATURE] #3 - PaymentDataSource insert 테스트용 코드 구현

--- app/src/main/java/co/kr/woowahan_accountbook/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/co/kr/woowahan_accountbook/presentation/ui/main/MainActivity.kt
@@ -1,10 +1,30 @@
 package co.kr.woowahan_accountbook.presentation.ui.main
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
 import co.kr.woowahan_accountbook.R
+import co.kr.woowahan_accountbook.data.datasource.local.payment.PaymentDataSource
 import co.kr.woowahan_accountbook.databinding.ActivityMainBinding
 import co.kr.woowahan_accountbook.presentation.ui.base.BaseActivity
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override val layoutRes: Int
         get() = R.layout.activity_main
+
+    @Inject
+    lateinit var paymentDataSource: PaymentDataSource
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        paymentDataSource.insertPayment("국민카드")
+    }
 }

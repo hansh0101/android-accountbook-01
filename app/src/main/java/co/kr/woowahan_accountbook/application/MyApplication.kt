51f726964c449fcd703594f1e7583e4956a5f174
hansh0101 <hansh0101@naver.com>
2022-07-26 22:51:55 +0900

    [FEATURE] #3 - DI 적용

--- app/src/main/java/co/kr/woowahan_accountbook/application/MyApplication.kt
+++ app/src/main/java/co/kr/woowahan_accountbook/application/MyApplication.kt
@@ -2,8 +2,10 @@ package co.kr.woowahan_accountbook.application
 
 import android.app.Application
 import androidx.viewbinding.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()

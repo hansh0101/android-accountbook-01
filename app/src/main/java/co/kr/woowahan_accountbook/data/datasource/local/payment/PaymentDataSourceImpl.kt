e2facdb134306f2bc2114e638386c6dce81a7a72
hansh0101 <hansh0101@naver.com>
2022-07-27 10:02:37 +0900

    [FEATURE] #3 - getPayment() 구현

--- app/src/main/java/co/kr/woowahan_accountbook/data/datasource/local/payment/PaymentDataSourceImpl.kt
+++ app/src/main/java/co/kr/woowahan_accountbook/data/datasource/local/payment/PaymentDataSourceImpl.kt
@@ -1,9 +1,7 @@
 package co.kr.woowahan_accountbook.data.datasource.local.payment
 
-import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import co.kr.woowahan_accountbook.data.dto.Payment
-import timber.log.Timber
 import javax.inject.Inject
 
 class PaymentDataSourceImpl @Inject constructor(
@@ -11,13 +9,22 @@ class PaymentDataSourceImpl @Inject constructor(
     private val writableDatabase: SQLiteDatabase
 ) : PaymentDataSource {
     override fun getPayment(id: Int): Payment {
-        val query: String =
-            "SELECT * FROM PAYMENT WHERE ID = '$id';"
-        val cursor = readableDatabase.rawQuery(query, null)
-        return Payment(
-            id = cursor.getInt(0),
-            paymentName = cursor.getString(1)
-        )
+        val projection = arrayOf("_ID", "PAYMENT_NAME")
+        val selection = "_ID = ?"
+        val selectionArgs = arrayOf("$id")
+        val sortOrder = "_ID DESC"
+
+        val cursor = readableDatabase.query(
+            "PAYMENT",
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            sortOrder
+        ).apply { moveToFirst() }
+
+        return Payment(cursor.getInt(0), cursor.getString(1))
     }
 
     override fun getPayments(): List<Payment> {
@@ -25,9 +32,9 @@ class PaymentDataSourceImpl @Inject constructor(
     }
 
     override fun insertPayment(name: String) {
-        val contentValues = ContentValues().apply {
-            put("PAYMENT_NAME", name)
-        }
+        val query: String =
+            "INSERT into PAYMENT (PAYMENT_NAME) values ('$name');"
+        writableDatabase.execSQL(query)
     }
 
     override fun updatePayment(id: Int, name: String) {

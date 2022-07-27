79d8d5431a78fa48d8222d08dfd51f41ba3cea92
hansh0101 <hansh0101@naver.com>
2022-07-27 10:33:52 +0900

    [FEATURE] #3 - ClassificationModule 구현

---
+++ app/src/main/java/co/kr/woowahan_accountbook/di/ClassificationModule.kt
@@ -0,0 +1,22 @@
+package co.kr.woowahan_accountbook.di
+
+import android.database.sqlite.SQLiteDatabase
+import co.kr.woowahan_accountbook.data.datasource.local.payment.ClassificationDataSource
+import co.kr.woowahan_accountbook.data.datasource.local.payment.ClassificationDataSourceImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ClassificationModule {
+    @Provides
+    @Singleton
+    fun provideClassificationDataSource(
+        @ReadableDatabase readableDatabase: SQLiteDatabase,
+        @WritableDatabase writableDatabase: SQLiteDatabase
+    ): ClassificationDataSource =
+        ClassificationDataSourceImpl(readableDatabase, writableDatabase)
+}

3991af62fb687e7726fbfb7a4a659d89957953ab
hansh0101 <hansh0101@naver.com>
2022-07-27 10:34:03 +0900

    [FEATURE] #3 - ClassificationDto data class 구현

---
+++ app/src/main/java/co/kr/woowahan_accountbook/data/dto/ClassificationDto.kt
@@ -0,0 +1,8 @@
+package co.kr.woowahan_accountbook.data.dto
+
+data class ClassificationDto(
+    val id: Int,
+    val classificationType: String,
+    val classificationColor: String,
+    val isIncome: Boolean
+)

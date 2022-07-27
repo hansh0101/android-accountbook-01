cf410d9954e9542fdf69733eacd46c84cd4ab7c5
hansh0101 <hansh0101@naver.com>
2022-07-27 10:40:19 +0900

    [DEL] #3 - 테스트용 코드 삭제

--- app/src/main/java/co/kr/woowahan_accountbook/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/co/kr/woowahan_accountbook/presentation/ui/main/MainActivity.kt
@@ -1,17 +1,12 @@
 package co.kr.woowahan_accountbook.presentation.ui.main
 
 import co.kr.woowahan_accountbook.R
-import co.kr.woowahan_accountbook.data.datasource.local.payment.PaymentDataSource
 import co.kr.woowahan_accountbook.databinding.ActivityMainBinding
 import co.kr.woowahan_accountbook.presentation.ui.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override val layoutRes: Int
         get() = R.layout.activity_main
-
-    @Inject
-    lateinit var paymentDataSource: PaymentDataSource
 }

98c324d0958ef6ee335fec020077e4c404698350
hansh0101 <hansh0101@naver.com>
2022-07-28 02:15:10 +0900

    [FEATURE] #9 - PaymentAdapter 생성

---
+++ app/src/main/java/co/kr/woowahan_accountbook/presentation/ui/main/setting/adapter/PaymentAdapter.kt
@@ -0,0 +1,27 @@
+package co.kr.woowahan_accountbook.presentation.ui.main.setting.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import co.kr.woowahan_accountbook.data.dto.PaymentDto
+
+class PaymentAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        const val HEADER = 0
+        const val BODY1 = 1
+        const val BODY2 = 2
+    }
+}

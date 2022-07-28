92999561276026d91a3cd803fe9926f76e62cd50
hansh0101 <hansh0101@naver.com>
2022-07-28 17:42:08 +0900

    [FEATURE] #11 - 결제수단 추가 UseCase 구현

---
+++ app/src/main/java/co/kr/woowahan_accountbook/domain/usecase/SettingPaymentAddUseCase.kt
@@ -0,0 +1,12 @@
+package co.kr.woowahan_accountbook.domain.usecase
+
+import co.kr.woowahan_accountbook.domain.repository.setting.SettingRepository
+import javax.inject.Inject
+
+class SettingPaymentAddUseCase @Inject constructor(
+    private val settingRepository: SettingRepository
+) {
+    suspend operator fun invoke(name: String) {
+        settingRepository.addPayment(name)
+    }
+}

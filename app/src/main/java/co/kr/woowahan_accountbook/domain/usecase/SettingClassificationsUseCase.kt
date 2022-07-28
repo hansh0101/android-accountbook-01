c67108c71ddea369e36dd7a87b53229ce6a8eb45
hansh0101 <hansh0101@naver.com>
2022-07-28 13:51:57 +0900

    [FEATURE] #9 - 설정 화면에 필요한 데이터를 가져올 UseCase 구현

---
+++ app/src/main/java/co/kr/woowahan_accountbook/domain/usecase/SettingClassificationsUseCase.kt
@@ -0,0 +1,18 @@
+package co.kr.woowahan_accountbook.domain.usecase
+
+import co.kr.woowahan_accountbook.domain.entity.setting.SettingClassification
+import co.kr.woowahan_accountbook.domain.repository.setting.SettingRepository
+import javax.inject.Inject
+
+class SettingClassificationsUseCase @Inject constructor(
+    private val settingRepository: SettingRepository
+) {
+    suspend operator fun invoke(isIncome: Boolean): List<SettingClassification> {
+        return settingRepository.getClassificationsByType(isIncome).map {
+            SettingClassification(SettingClassification.Type.BODY, it)
+        }.toMutableList().apply {
+            add(0, SettingClassification(SettingClassification.Type.HEADER, isIncome))
+            add(SettingClassification(SettingClassification.Type.FOOTER, isIncome))
+        }
+    }
+}
---
+++ app/src/main/java/co/kr/woowahan_accountbook/domain/usecase/SettingPaymentsUseCase.kt
@@ -0,0 +1,20 @@
+package co.kr.woowahan_accountbook.domain.usecase
+
+import co.kr.woowahan_accountbook.domain.entity.setting.SettingPayment
+import co.kr.woowahan_accountbook.domain.repository.setting.SettingRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class SettingPaymentsUseCase @Inject constructor(
+    private val settingRepository: SettingRepository
+) {
+    suspend operator fun invoke(): List<SettingPayment> {
+        return settingRepository.getPayments().map {
+            SettingPayment(SettingPayment.Type.BODY, it)
+        }.toMutableList().apply {
+            add(0, SettingPayment(SettingPayment.Type.HEADER, 0, ""))
+            add(SettingPayment(SettingPayment.Type.FOOTER, 0, ""))
+        }
+    }
+}

c3dc2c52babbe0bb0171f09e9af19785b955b6af
hansh0101 <hansh0101@naver.com>
2022-07-28 13:51:22 +0900

    [FEATURE] #9 - CoroutineDispatcher 주입을 위한 CoroutineModule 구현

---
+++ app/src/main/java/co/kr/woowahan_accountbook/di/CoroutineModule.kt
@@ -0,0 +1,41 @@
+package co.kr.woowahan_accountbook.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutineModule {
+    @MainDispatcher
+    @Provides
+    @Singleton
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+
+    @IoDispatcher
+    @Provides
+    @Singleton
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @DefaultDispatcher
+    @Provides
+    @Singleton
+    fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+}
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class MainDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DefaultDispatcher

b74eb09f6ace717ccb65e0ba4537af2bafdc13e5
hansh0101 <hansh0101@naver.com>
2022-07-28 17:42:15 +0900

    [FEATURE] #11 - 결제수단 업데이트 UseCase 구현

---
+++ app/src/main/java/co/kr/woowahan_accountbook/domain/usecase/SettingPaymentUpdateUseCase.kt
@@ -0,0 +1,13 @@
+package co.kr.woowahan_accountbook.domain.usecase
+
+import co.kr.woowahan_accountbook.domain.repository.setting.SettingRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
+
+class SettingPaymentUpdateUseCase @Inject constructor(
+    private val settingRepository: SettingRepository
+) {
+    suspend operator fun invoke(id: Int, name: String) {
+        settingRepository.updatePayment(id, name)
+    }
+}

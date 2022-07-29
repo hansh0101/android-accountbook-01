98589b3b03d8bdd11135bcb37242f607e00dc701
hansh0101 <hansh0101@naver.com>
2022-07-29 11:28:59 +0900

    [FEATURE] #13 - 분류 업데이트 UseCase 구현

---
+++ app/src/main/java/co/kr/woowahan_accountbook/domain/usecase/SettingClassificationUpdateUseCase.kt
@@ -0,0 +1,12 @@
+package co.kr.woowahan_accountbook.domain.usecase
+
+import co.kr.woowahan_accountbook.domain.repository.setting.SettingRepository
+import javax.inject.Inject
+
+class SettingClassificationUpdateUseCase @Inject constructor(
+    private val settingRepository: SettingRepository
+) {
+    suspend operator fun invoke(id: Int, type: String, color: String, isIncome: Boolean) {
+        settingRepository.updateClassification(id, type, color, isIncome)
+    }
+}

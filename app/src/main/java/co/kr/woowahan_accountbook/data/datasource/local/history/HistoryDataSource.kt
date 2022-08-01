e67d79ce58f32bffb1d1616a8ec656ab6aac5f67
hansh0101 <hansh0101@naver.com>
2022-08-02 01:17:13 +0900

    [FEATURE] #21 - 수입/지출 필터를 위한 인자 추가

--- app/src/main/java/co/kr/woowahan_accountbook/data/datasource/local/history/HistoryDataSource.kt
+++ app/src/main/java/co/kr/woowahan_accountbook/data/datasource/local/history/HistoryDataSource.kt
@@ -4,7 +4,13 @@ import co.kr.woowahan_accountbook.domain.entity.dto.HistoryDto
 
 interface HistoryDataSource {
     fun getHistory(id: Int): HistoryDto
-    fun getHistories(year: Int, month: Int): List<HistoryDto>
+    fun getHistories(
+        year: Int,
+        month: Int,
+        incomeFlag: Boolean,
+        expenditureFlag: Boolean
+    ): List<HistoryDto>
+
     fun insertHistory(
         amount: Int,
         description: String,

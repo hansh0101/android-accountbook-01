bd513cbf2c0eadac8bfbc9f497e8a929a16142e0
hansh0101 <hansh0101@naver.com>
2022-08-01 19:24:49 +0900

    [FEATURE] #17 - 내역 등록 화면 분류 Spinner Adapter 구현

--- app/src/main/java/co/kr/woowahan_accountbook/presentation/adapter/history/HistoryClassificationSpinnerAdapter.kt
+++ app/src/main/java/co/kr/woowahan_accountbook/presentation/adapter/history/HistoryClassificationSpinnerAdapter.kt
@@ -1,6 +1,5 @@
 package co.kr.woowahan_accountbook.presentation.adapter.history
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,41 +9,58 @@ import co.kr.woowahan_accountbook.R
 import co.kr.woowahan_accountbook.databinding.ItemHistoryAddSpinnerBinding
 import co.kr.woowahan_accountbook.domain.entity.dto.ClassificationDto
 
-class HistoryClassificationSpinnerAdapter(
-    private val context: Context,
-    private val list: List<ClassificationDto>
-) :
-    BaseAdapter() {
-    override fun getCount(): Int = list.size + 1
+class HistoryClassificationSpinnerAdapter(isIncome: Boolean) : BaseAdapter() {
+    private var items = listOf<ClassificationDto>(
+        ClassificationDto(0, "추가하기", "", isIncome),
+        ClassificationDto(0, "선택하세요", "", isIncome)
+    )
 
-    override fun getItem(position: Int): ClassificationDto? {
-        return if (position == list.size) {
-            null
-        } else {
-            list[position]
-        }
-    }
+    override fun getCount(): Int = if (items.isEmpty()) 0 else items.size
+
+    override fun getItem(position: Int): ClassificationDto = items[position]
 
     override fun getItemId(position: Int): Long = position.toLong()
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val binding = ItemHistoryAddSpinnerBinding.inflate(
-            LayoutInflater.from(context),
+            LayoutInflater.from(parent?.context),
             parent,
             false
         )
 
-        if (position == list.size + 1) {
-            with(binding.tvLabel) {
-                hint = "선택하세요"
-                setHintTextColor(resources.getColor(R.color.light_purple_a79fcb, null))
-            }
-        } else if (position == list.size) {
-            binding.tvLabel.text = "추가하기"
+        binding.tvLabel.text = getItem(position).classificationType
+        if (position == count - 1) {
+            binding.root.layoutParams.height = 1
+        } else if (position == count - 2) {
             binding.ivPlus.isVisible = true
-        } else {
-            binding.tvLabel.text = list[position].classificationType
         }
         return binding.root
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val binding = ItemHistoryAddSpinnerBinding.inflate(
+            LayoutInflater.from(parent?.context),
+            parent,
+            false
+        )
+
+        if (position == items.size - 1) {
+            with(binding.tvLabel) {
+                hint = items[position].classificationType
+                setHintTextColor(resources.getColor(R.color.light_purple_a79fcb, null))
+            }
+        } else {
+            binding.tvLabel.text = items[position].classificationType
+        }
+        return binding.root
+    }
+
+    override fun isEnabled(position: Int): Boolean {
+        return position != count - 1
+    }
+
+    fun updateItems(newItems: List<ClassificationDto>) {
+        items = newItems
+        notifyDataSetChanged()
     }
 }

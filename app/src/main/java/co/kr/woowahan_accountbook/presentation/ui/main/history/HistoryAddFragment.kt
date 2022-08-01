3be87478c914a80bb5de5a2ad2aa58837dfbe55f
hansh0101 <hansh0101@naver.com>
2022-08-01 19:24:33 +0900

    [FEATURE] #17 - 내역 등록 화면 로직 구현

--- app/src/main/java/co/kr/woowahan_accountbook/presentation/ui/main/history/HistoryAddFragment.kt
+++ app/src/main/java/co/kr/woowahan_accountbook/presentation/ui/main/history/HistoryAddFragment.kt
@@ -1,17 +1,30 @@
 package co.kr.woowahan_accountbook.presentation.ui.main.history
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.AdapterView
 import android.widget.Spinner
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import androidx.fragment.app.viewModels
 import co.kr.woowahan_accountbook.R
 import co.kr.woowahan_accountbook.databinding.FragmentHistoryAddBinding
-import co.kr.woowahan_accountbook.domain.entity.dto.ClassificationDto
-import co.kr.woowahan_accountbook.domain.entity.dto.PaymentDto
 import co.kr.woowahan_accountbook.presentation.adapter.history.HistoryClassificationSpinnerAdapter
 import co.kr.woowahan_accountbook.presentation.adapter.history.HistoryPaymentSpinnerAdapter
 import co.kr.woowahan_accountbook.presentation.ui.base.BaseFragment
+import co.kr.woowahan_accountbook.presentation.ui.main.setting.ClassificationAddFragment
+import co.kr.woowahan_accountbook.presentation.ui.main.setting.PaymentAddFragment
 import co.kr.woowahan_accountbook.presentation.ui.widget.HistoryAddPaymentSpinner
+import co.kr.woowahan_accountbook.presentation.viewmodel.main.history.HistoryAddViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import java.text.SimpleDateFormat
+import java.util.*
 
+
+@AndroidEntryPoint
 class HistoryAddFragment : BaseFragment<FragmentHistoryAddBinding>(),
     HistoryAddPaymentSpinner.OnSpinnerEventsListener {
     override val TAG: String
@@ -19,45 +32,123 @@ class HistoryAddFragment : BaseFragment<FragmentHistoryAddBinding>(),
     override val layoutRes: Int
         get() = R.layout.fragment_history_add
 
-    private val historyPaymentSpinnerAdapter by lazy {
-        HistoryPaymentSpinnerAdapter(
-            requireContext(),
-            listOf(PaymentDto(1, "국민은행 체크카드"), PaymentDto(2, "카카오뱅크"))
-        )
-    }
+    private val historyPaymentSpinnerAdapter by lazy { HistoryPaymentSpinnerAdapter() }
 
     private val historyClassificationSpinnerAdapter by lazy {
-        HistoryClassificationSpinnerAdapter(
-            requireContext(),
-            listOf(
-                ClassificationDto(1, "식비", "#CB588F", false),
-                ClassificationDto(2, "교육비", "#9BD182", true)
-            )
-        )
+        HistoryClassificationSpinnerAdapter(isIncome = true)
     }
+
+    private val viewModel by viewModels<HistoryAddViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewmodel = viewModel
         initView()
+        observeData()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initData()
     }
 
     private fun initView() {
+        initToolbar()
+        initPaymentSpinner()
+        initClassificationSpinner()
+        initDateValue()
+        binding.btnAdd.setOnClickListener {
+            viewModel.insertHistory()
+        }
+    }
+
+    private fun initData() {
+        with(viewModel) {
+            getPayments()
+            getClassifications(true)
+        }
+    }
+
+    private fun initToolbar() {
         with(binding.tbHistoryAdd) {
             setNavigationIcon(co.kr.woowahan_accountbook.R.drawable.ic_back)
             setNavigationOnClickListener {
                 requireActivity().onBackPressed()
             }
         }
+    }
+
+    private fun initPaymentSpinner() {
         with(binding.spinnerPaymentValue) {
             setSpinnerEventsListener(this@HistoryAddFragment)
             adapter = historyPaymentSpinnerAdapter
-            setSelection(3)
+            setSelection(historyPaymentSpinnerAdapter.count - 1)
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    if (position == historyPaymentSpinnerAdapter.count - 2) {
+                        parentFragmentManager.commit {
+                            replace<PaymentAddFragment>(R.id.fcv_main)
+                            addToBackStack(PaymentAddFragment::class.java.simpleName)
+                        }
+                    } else {
+                        viewModel.onSelectedPaymentItem(
+                            historyPaymentSpinnerAdapter.getItem(position).id
+                        )
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+            }
         }
+    }
+
+    private fun initClassificationSpinner() {
         with(binding.spinnerClassificationValue) {
             setSpinnerEventsListener(this@HistoryAddFragment)
             adapter = historyClassificationSpinnerAdapter
-            setSelection(3)
+            setSelection(historyClassificationSpinnerAdapter.count - 1)
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    if (position == historyClassificationSpinnerAdapter.count - 2) {
+                        parentFragmentManager.commit {
+                            replace<ClassificationAddFragment>(
+                                R.id.fcv_main,
+                                null,
+                                Bundle().apply { putInt("IS_INCOME", 1) })
+                            addToBackStack(ClassificationAddFragment::class.java.simpleName)
+                        }
+                    } else {
+                        viewModel.onSelectedClassificationItem(
+                            historyClassificationSpinnerAdapter.getItem(position).id
+                        )
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+            }
         }
+    }
+
+    private fun initDateValue() {
+        val calendar = Calendar.getInstance()
+        val time = calendar.time
+        val simpleDateFormat = SimpleDateFormat("yyyy.MM.dd.E요일")
+        val date = simpleDateFormat.format(time)
+        viewModel.setDate(date)
     }
 
     override fun onPopupWindowOpened(spinner: Spinner?) {
@@ -72,5 +163,70 @@ class HistoryAddFragment : BaseFragment<FragmentHistoryAddBinding>(),
             it.background =
                 resources.getDrawable(R.drawable.bg_spinner_history_add, null)
         }
+    }
+
+    private fun observeData() {
+        addTextChangedListener()
+
+        viewModel.payments.observe(viewLifecycleOwner) {
+            Timber.tag("payments size").i(it.size.toString())
+            historyPaymentSpinnerAdapter.updateItems(it)
+            binding.spinnerPaymentValue.setSelection(it.size)
+        }
+
+        viewModel.classifications.observe(viewLifecycleOwner) {
+            Timber.tag("classifications size").i(it.size.toString())
+            historyClassificationSpinnerAdapter.updateItems(it)
+            binding.spinnerClassificationValue.setSelection(it.size)
+        }
+
+        viewModel.amount.observe(viewLifecycleOwner) {
+            Timber.tag("amount observe").i(it.toString())
+        }
+
+        viewModel.date.observe(viewLifecycleOwner) {
+            Timber.tag("date observe").i(it)
+        }
+
+        viewModel.payment.observe(viewLifecycleOwner) {
+            Timber.tag("payment observe").i(it.toString())
+        }
+
+        viewModel.isSuccess.observe(viewLifecycleOwner) {
+            requireActivity().onBackPressed()
+        }
+    }
+
+    private fun addTextChangedListener() {
+        binding.etAmountValue.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (binding.etAmountValue.text == null || binding.etAmountValue.text.toString() == "") {
+                    return
+                }
+                binding.etAmountValue.removeTextChangedListener(this)
+
+                val value = binding.etAmountValue.text.toString().replace(",", "")
+                val parsed: String = "%,d".format(value.toInt())
+
+                binding.etAmountValue.setText(parsed)
+                binding.etAmountValue.setSelection(parsed.length)
+                binding.etAmountValue.addTextChangedListener(this)
+
+                if (binding.etAmountValue.text == null || binding.etAmountValue.text.toString() == "") {
+                    viewModel.amount.value = 0
+                } else {
+                    viewModel.amount.value =
+                        binding.etAmountValue.text.toString().replace(",", "").toInt()
+                }
+            }
+        })
     }
 }

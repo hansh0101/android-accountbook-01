38fb6ea039ce7c6413d6885dc0dd96d434f35832
hansh0101 <hansh0101@naver.com>
2022-08-01 19:27:45 +0900

    [FEATURE] #17 - HistoryRepository 구현체 구현

--- app/src/main/java/co/kr/woowahan_accountbook/data/repository/history/HistoryRepositoryImpl.kt
+++ app/src/main/java/co/kr/woowahan_accountbook/data/repository/history/HistoryRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package co.kr.woowahan_accountbook.data.repository.history
 
+import co.kr.woowahan_accountbook.data.datasource.local.classification.ClassificationDataSource
 import co.kr.woowahan_accountbook.data.datasource.local.history.HistoryDataSource
+import co.kr.woowahan_accountbook.data.datasource.local.payment.PaymentDataSource
+import co.kr.woowahan_accountbook.domain.entity.dto.ClassificationDto
 import co.kr.woowahan_accountbook.domain.entity.dto.HistoryDto
-import co.kr.woowahan_accountbook.domain.entity.history.HistoryItem
+import co.kr.woowahan_accountbook.domain.entity.dto.PaymentDto
 import co.kr.woowahan_accountbook.domain.repository.history.HistoryRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
@@ -10,10 +13,42 @@ import javax.inject.Inject
 
 class HistoryRepositoryImpl @Inject constructor(
     private val historyDataSource: HistoryDataSource,
+    private val paymentDataSource: PaymentDataSource,
+    private val classificationDataSource: ClassificationDataSource,
     private val coroutineDispatcher: CoroutineDispatcher
 ) : HistoryRepository {
     override suspend fun getHistories(year: Int, month: Int): List<HistoryDto> =
         withContext(coroutineDispatcher) {
             historyDataSource.getHistories(year, month)
+        }
+
+    override suspend fun getPayments(): List<PaymentDto> =
+        withContext(coroutineDispatcher) {
+            paymentDataSource.getPayments()
+        }
+
+    override suspend fun insertHistory(
+        amount: Int,
+        description: String,
+        year: Int,
+        month: Int,
+        day: Int,
+        paymentId: Int,
+        classificationId: Int
+    ) = withContext(coroutineDispatcher) {
+        historyDataSource.insertHistory(
+            amount,
+            description,
+            year,
+            month,
+            day,
+            paymentId,
+            classificationId
+        )
+    }
+
+    override suspend fun getClassifications(): List<ClassificationDto> =
+        withContext(coroutineDispatcher) {
+            classificationDataSource.getClassifications()
         }
 }
